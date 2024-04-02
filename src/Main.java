1a2e6a877c98e7df405df0402655263c3337a1b5
Joel Alamillo <alamillojuarez@gmail.com>
2024-04-01 21:17:41 -0600

    Version 1 Como usar operaciones basicas en Java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int a = 3, b=2;
+        var resultado = a + b;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("Resultado suma a + b : " + resultado);
+
+        resultado = a - b;
+        System.out.println("resultado resta a - b = " + resultado);
+
+        resultado = a * b;
+        System.out.println("resultado multiplicacion a * b = " + resultado);
+
+        var resultado2 = 3D / b;
+        System.out.println("resultado division a / b = " + resultado2);
+     }
 }
